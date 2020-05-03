bcd917f3da673ee34cd1903e15b3e3cc7e23d11a
Mara Schulke <mara@schulke.xyz>
2020-05-03 11:56:53 +0200

    rewrite parts of the program to simplify it

--- tables.hs
+++ tables.hs
@@ -7,6 +7,15 @@ import Data.List.Split
 factorial :: Int -> Int
 factorial n = product [1..n]
 
+-- The carthesian product of two lists
+carthesianProduct :: [a] -> [a] -> [[a]]
+carthesianProduct xs ys = [[x] ++ [y] | x <- xs, y <- ys ]
+
+-- Combine all values from the first list and the second list to generate pairs
+-- ['X', 'Y'] ['A', 'B'] would return [['A','B','X'], ['A','B','Y']]
+combine :: [a] -> [a] -> [[a]]
+combine v l = [l ++ [x] | x <- v]
+
 -- The binomial coefficient function
 binomialCoefficient :: Int -> Int -> Int
 binomialCoefficient n k | k > n = 0
@@ -77,34 +86,28 @@ amountOfGamedays = ceiling ((fromIntegral games) / (fromIntegral gamesPerDay))
 
 -- All combinations of teams
 combinationsOfTeams :: [(Team, Team)]
-combinationsOfTeams = map (\x -> (x !! 0, x !! 1)) [x| x <- mapM (const teams) [1..2], head x < head (tail x) ]
+combinationsOfTeams = map (\x -> (x !! 0, x !! 1)) ([x| x <- mapM (const teams) [1..2], head x < head (tail x) ])
 
 -- All possible Results for a game
 possibleResults :: [(Point, Point)]
 possibleResults = [(win, lose), (lose, win), (draw, draw)]
 
--- fst (allCombos !! 0) == combinationOfTeams !! 0
-allCombos :: [[Game]]
-allCombos = map (\team -> map (\res -> Game team res) possibleResults) combinationsOfTeams
+-- All combinations of matches and results
+-- The first item holds all possible results for the first match eg. "A vs B" and so on
+allCombinations :: [[Game]]
+allCombinations = map (\team -> map (\res -> Game team res) possibleResults) combinationsOfTeams
 
-carthesianProduct :: [Game] -> [Game] -> [[Game]]
-carthesianProduct xs ys = [[x] ++ [y] | x <- xs, y <- ys ]
+-- Generates all possible streaks
+-- It takes all possible Values, the offset to the actual next values in the possible Values list, and base to append to
+generateAllPossibleStreaks :: [[Game]] -> Int -> [[Game]] -> [[Game]]
+generateAllPossibleStreaks pv offset xs =
+                if (length pv) - 1 == offset then ys
+                else generateAllPossibleStreaks pv (offset + 1) ys
+                where ys = concat (map (combine (allCombinations !! offset)) xs)
 
--- Combine all values from the first list and the second list to generate pairs
--- ['X', 'Y'] ['A', 'B'] would return [['A','B','X'], ['A','B','Y']]
-combine :: [Game] -> [Game] -> [[Game]]
-combine v l = [l ++ [x] | x <- v]
-
--- THIS SHOULD BE RECURSIVE BUT I AM TO DUMB TO DO THAT --
-start = carthesianProduct (allCombos !! 0) (allCombos !! 1)
-level2 = concat (map (combine (allCombos !! 2)) start)
-level3 = concat (map (combine (allCombos !! 3)) level2)
-level4 = concat (map (combine (allCombos !! 4)) level3)
-level5 = concat (map (combine (allCombos !! 5)) level4)
-----------------------------------------------------------
-
+-- The genereated possible streaks
 possibleStreaks :: [[Game]]
-possibleStreaks = level5
+possibleStreaks = generateAllPossibleStreaks allCombinations 0 [[]]
 
 possibleGamedayStreaks :: [[Gameday]]
 possibleGamedayStreaks = map (chunksOf gamesPerDay) possibleStreaks
@@ -120,11 +123,11 @@ pointsForTeam t g =
                 else if t == snd tn then snd res
                 else 0
 
-filterGamesWithTeam :: Team -> [Game] -> [Game]
-filterGamesWithTeam t games = filter (teamParticipatedInGame t) games
+filterGamesByTeam :: Team -> [Game] -> [Game]
+filterGamesByTeam t games = filter (teamParticipatedInGame t) games
 
 sumPointsForTeam :: Team -> [Game] -> Point
-sumPointsForTeam t g = (sum (map (pointsForTeam t) (filterGamesWithTeam t g)))
+sumPointsForTeam t g = (sum (map (pointsForTeam t) (filterGamesByTeam t g)))
 
 gamedaysToTeamResult :: [Gameday] -> [TeamResult]
 gamedaysToTeamResult gamedays = reverse (sort (map (\team -> TeamResult team (sumPointsForTeam team (concat gamedays))) teams))
@@ -139,44 +142,3 @@ printTables = do
 main :: IO ()
 main = do
     printTables
-
-----------------------------------------------------------------------------
--- type TableEntry = (Point, [Point])
--- type Table = [TableEntry]
-
--- The amount of points per table is relative to the amount of games and the points for a game
--- pointsPerTable :: Point
--- pointsPerTable = (fromIntegral games) * win
-
--- -- Append all values from the first list to the second list 
--- append :: [Point] -> [Point] -> [[Point]]
--- append v l = [l ++ [x] | x <- v]
-
--- -- Create pairs of 2 lists
--- getPairs :: [Point] -> [Point] -> [[Point]]
--- getPairs v l = concat (map (append v) [l])
-
--- -- Create all combinations with given values to a given level
--- getCombinations :: Int -> [Point] -> [[Point]] -> [[Point]]
--- getCombinations level values start = 
---     if (level - 1) == 0 then y
---     else getCombinations (level - 1) values y
---     where y = concat (map (getPairs values) start)
-
--- -- All possible values for a team in a table
--- possibleValues :: [Point]
--- possibleValues = map (*0.5) (take (games + 1) [0..])
--- This is the right implementation tracking the different play days.
--- toTableEntry :: [Point] -> TableEntry
--- toTableEntry p = (sum p, p)
--- This is just a temp solution ignoring the single games
--- toTableEntry :: Point -> TableEntry
--- toTableEntry p = (p, [])
-
-
--- allUnsortedTables :: [[Point]]
--- allUnsortedTables = getCombinations (teams - 1) possibleValues [[x] | x <- possibleValues]
-
--- tables :: [[Point]]
--- tables = 
---     map sort (filter (\x -> sum x == pointsPerTable) allUnsortedTables)
